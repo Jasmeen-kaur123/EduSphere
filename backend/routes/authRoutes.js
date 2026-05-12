c9d172739ca7b614dd92279500ab3aad8986d792
Randeep Singh <randeep2720@gmail.com>
2026-05-12 11:31:55 +0530

    updated authentication routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { signup, login, getMe } = require("../controllers/authController");
-
 const { getProfile } = require('../controllers/authController')
 
 router.post("/signup", signup);
 router.post("/login", login);
-
 router.get('/me', require('../middleware/authMiddleware').protect, getMe)
 
 module.exports = router;

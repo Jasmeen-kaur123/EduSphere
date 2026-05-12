5a48e172d8c29a27c6ac93d8bc9c01312329f921
Jasmeen-kaur123 <157448132+Jasmeen-kaur123@users.noreply.github.com>
2026-05-12 09:05:10 +0530

    Delete backend directory

--- backend/src/models/Testimonial.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const testimonialSchema = new mongoose.Schema({
-  name: String,
-  message: String
-});
-
-module.exports = mongoose.model("Testimonial", testimonialSchema);
